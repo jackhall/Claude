57a5e5e38f25c8a2e339f973474f956126e3ea60
jackhall <jackwhall7@gmail.com>
2012-01-27 01:23:09 -0600

    created files, added Benoit as submodule

---
+++ src/InfoNet.h
@@ -0,0 +1,10 @@
+#ifndef InfoNet_h
+#define InfoNet_h
+
+#include "Benoit.h"
+
+class InfoNet {
+
+};
+
+#endif
---
+++ src/InfoNeuron.h
@@ -0,0 +1,10 @@
+#ifndef InfoNeuron_h
+#define InfoNeuron_h
+
+#include "Benoit.h"
+
+class InfoNeuron {
+
+};
+
+#endif
